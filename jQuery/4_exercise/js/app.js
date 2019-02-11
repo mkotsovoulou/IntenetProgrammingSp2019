dfaf307127af345aae266adeabc0a3ec3a459f44
mairako <mkotsovoulou@acg.edu>
2019-02-11 20:29:20 +0200

    register

--- jQuery/4_exercise/js/app.js
+++ jQuery/4_exercise/js/app.js
@@ -2,8 +2,31 @@
 //document.getElementById("flashMessage").style.display = "none";
 
 $("#flashMessage").hide();
+$("#email-area").hide();
 
-$("#previewButton").click(function(){  //callback function
+$("#username").focusout(function(){
+   if ($('#username').val().length < 6) {
+     $('#wrongusername').text("Username must be more than 6 characters");
+   } 
+  else 
+     $('#wrongusername').text("");
+});
+$("#password2").focusout(function(){
+   if ($('#password').val() != $('#password2').val()) {
+     $('#wrongpassword').text("Passwords do not match");
+   } 
+  else 
+     $('#wrongpassword').text("");
+});
+
+$("#subscribe").change( function(){
+   if( $(this).is(':checked') )  
+     $("#email-area").fadeIn(1000);
+  else
+    $("#email-area").fadeOut(1000);
+});
+
+/* $("#previewButton").click(function(){  //callback function
   var x = $('#blogTitleInput').val();
   var y = $('#blogContentInput').val();
 
@@ -13,4 +36,6 @@ $("#previewButton").click(function(){  //callback function
   $("#flashMessage").fadeIn(2000).delay(3000).slideUp();
 
 });
+
+*/
 
